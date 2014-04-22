30eea2adffe2059b9376e2ef7c00123722e3563a
Milannic <milannic.cheng.liu@gmail.com>
2014-04-22 00:58:21 -0400

    backtrace

--- c_test/test.c
+++ c_test/test.c
@@ -18,7 +18,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
+#include<execinfo.h>
+#define SIZE 20
 
+int haha(int level){
+	int ret;
+	int size = SIZE;
+	void * EIP[SIZE];
+	void * SYMBOL[SIZE];
+
+	ret = backtrace(EIP,size);
+	char ** str = backtrace_symbols(SYMBOL,ret);
+
+
+		
+	printf("level = %d\n",level);
+
+	for (int i =0;i<ret;i++){
+		printf("%p\n",EIP[i]);
+		printf("%s\n",str[i]);
+	}
+	printf("-------------------\n");
+	printf("%p\n",EIP[2]);
+	printf("%s\n",str[2]);
+	printf("-------------------\n");
+	if(level>0){
+		ret = haha(level-1);
+	}
+	free(str);
+	return 0;
+}
 
 	int
 main ( int argc, char *argv[] )
@@ -28,5 +57,6 @@ main ( int argc, char *argv[] )
 	if("hahaha"){
 		printf("this is always true\n");
 	}
+	a = haha(5);
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
